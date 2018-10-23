c38060a130707fdabf0f04a15454a50040c028e6
mark <snowtowind@yahoo.com>
2018-10-22 21:13:48 -0700

    sdf

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 
 let sno;let can;
-
+let hr1;
 let ob;
 function setup() {
 row=500;col=600;rectMode(CENTER);
@@ -11,7 +11,8 @@ sno=select('#snow');sno.position(40,50);
 sno.class('inpbdd');
 ob=new object(200,50,3,2);
 startup();
-
+hr1=select('#hr');hr1.position(50,120);
+hr1.class('inpbdd');
 text("10011001 or 10100101 or 10110110", 20,100);
 
 }
